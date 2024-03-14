88b13336720ebd52d29a9b686d71d2bf49c12ea1
Fabio Alessandro Locati <me@fale.io>
2024-03-14 20:41:05 +0100

    Add capability to get data from FTPs

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.21.7
 
 require (
 	github.com/gorilla/mux v1.8.1
+	github.com/jlaffaye/ftp v0.2.0
 	github.com/urfave/cli/v2 v2.27.1
 	github.com/urfave/negroni v1.0.0
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- pkg/gtfs/download.go
+++ pkg/gtfs/download.go
@@ -7,9 +7,12 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"time"
+
+	"github.com/jlaffaye/ftp"
 )
 
 func (g *GTFS) Download(maxAge time.Duration, force bool) error {
@@ -25,26 +28,24 @@ func (g *GTFS) Download(maxAge time.Duration, force bool) error {
 	}
 
 	slog.Info(fmt.Sprintf("download %v", g.SourceURL))
-	req, err := http.NewRequest("GET", g.SourceURL, nil)
+	u, err := url.Parse(g.SourceURL)
 	if err != nil {
 		return err
 	}
-
-	req.Header.Set("User-Agent", "GTFS Proxy/0.1")
-	req.Header.Set("If-Modified-Since", g.LastModified.Format(time.RFC1123))
-
-	c := http.Client{Timeout: time.Duration(10) * time.Second}
-	resp, err := c.Do(req)
+	var body []byte
+	switch u.Scheme {
+	case "ftp":
+		body, err = downloadFTP(g.SourceURL, g.LastModified)
+	case "http", "https":
+		body, err = downloadHTTP(g.SourceURL, g.LastModified)
+	}
 	if err != nil {
 		g.LastDownloadAttempted = now
 		g.Save(g.location)
 		return err
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
 
-	sum256 := sha256.Sum256(body)
-	if base64.URLEncoding.EncodeToString(sum256[:]) == g.Digest.SHA256 {
+	if body == nil {
 		slog.Debug(
 			fmt.Sprintf("%v not saved, already up to date", g.ID),
 			"sha256", g.Digest.SHA256,
@@ -65,6 +66,18 @@ func (g *GTFS) Download(maxAge time.Duration, force bool) error {
 		return nil
 	}
 
+	sum256 := sha256.Sum256(body)
+	if base64.URLEncoding.EncodeToString(sum256[:]) == g.Digest.SHA256 {
+		slog.Debug(
+			fmt.Sprintf("%v not saved, already up to date", g.ID),
+			"sha256", g.Digest.SHA256,
+		)
+		g.LastSuccessfulDownload = now
+		g.LastDownloadAttempted = now
+		g.Save(g.location)
+		return nil
+	}
+
 	if err := os.WriteFile(path.Join(g.location, fmt.Sprintf("%v.zip", g.ID)), body, 0o644); err != nil {
 		return err
 	}
@@ -75,4 +88,67 @@ func (g *GTFS) Download(maxAge time.Duration, force bool) error {
 	g.Save(g.location)
 
 	return nil
+}
+
+func downloadHTTP(uri string, lastModified time.Time) ([]byte, error) {
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "GTFS Proxy/0.1")
+	req.Header.Set("If-Modified-Since", lastModified.Format(time.RFC1123))
+
+	c := http.Client{Timeout: time.Duration(10) * time.Second}
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotModified {
+		return nil, nil
+	}
+	return io.ReadAll(resp.Body)
+}
+
+func downloadFTP(uri string, lastModified time.Time) ([]byte, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := ftp.Dial(fmt.Sprintf("%s:21", u.Host), ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		return nil, err
+	}
+
+	user := "anonymous"
+	pass := "anonymous"
+	if u.User != nil {
+		user = u.User.Username()
+		if p, ok := u.User.Password(); ok {
+			pass = p
+		}
+	}
+	err = c.Login(user, pass)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.IsGetTimeSupported() {
+		// Implement https://pkg.go.dev/github.com/jlaffaye/ftp#ServerConn.GetTime
+		t, err := c.GetTime(u.Path)
+		if err == nil && t.Before(lastModified) {
+			return nil, nil
+		}
+	}
+
+	r, err := c.Retr(u.Path)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	buf, err := io.ReadAll(r)
+	return buf, err
 }
